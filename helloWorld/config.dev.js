00256fae0474ffc58b57b94d4af5e83db3db9fef
GucciGerm <germaine.balmania@gmail.com>
2018-09-14 02:50:28 +0000

    Adding node mods

---
+++ helloWorld/config.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  global: { authAccess: { accessToken: '5c7cd898b8e7ce0b3b5ef57bd3982918bc97b2e0' } },
+  // IntentName: { customField: 'Custom Value that will be added to the context' }
+}
